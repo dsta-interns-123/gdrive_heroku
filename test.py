80b0dcbc7c3a0d9f13cb81099c533b337766c351
dsta-interns-123 <46216033+dsta-interns-123@users.noreply.github.com>
2019-02-27 12:07:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -58,12 +58,12 @@ def processRequest(req):
     wks.update_cell(121,1,len(list_files[1]))
     wks.update_cell(122,1,folder_name)
     wks.update_cell(123,1,list_files[0][0])
-    wks.update_cell(124,1,list_file[0][1])
+    wks.update_cell(124,1,list_files[0][1])
     
     for item in list_files[0]:       
         position = list_files[0].index(item)
         file_name = list_files[0][position]
-        file_id = list_files[1][position]
+        #file_id = list_files[1][position]
         #request = service.files().get_media(fileId=file_id) #to edit so can read batch files
         wks.update_cell(125,1,file_name)
         wks.update_cell(126,1,"bad")
